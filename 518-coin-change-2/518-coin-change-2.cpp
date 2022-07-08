63dcb775b32f89371f092dce1ded7d3ce58fb63b
SatvikSingh <64217296+SatvikSingh@users.noreply.github.com>
2022-07-08 11:40:26 +0530

    Time: 73 ms (23.01%), Space: 7.2 MB (89.21%) - LeetHub

---
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        
+        vector<int> dp(amount+1, 0);
+        dp[0] = 1;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j <= amount; j++) {
+                if (coins[i] <= j) {
+                    dp[j] += dp[j - coins[i]];
+                }
+            }
+        }
+        
+        for (auto i : dp) {
+            cout << i << " ";
+        }
+        
+        return dp.back();
+    }
+};
