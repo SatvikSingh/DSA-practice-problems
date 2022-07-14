d0982ce5bee9d28d2fc89a7b1fa58860d24e2138
SatvikSingh <64217296+SatvikSingh@users.noreply.github.com>
2022-07-14 12:30:33 +0530

    Time: 8 ms (90.56%), Space: 20.2 MB (65.08%) - LeetHub

---
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int helper(TreeNode* root, int& ans) {
+        if (!root)
+            return 0;
+        
+        int left = helper(root->left, ans);
+        int right = helper(root->right, ans);
+        
+        ans = max(ans, left+right+1);
+        return 1 + max(left, right);
+    }
+    
+    int diameterOfBinaryTree(TreeNode* root) {
+        int ans = 0;
+        if (!root)
+            return 0;
+        
+        helper(root, ans);
+        
+        return (ans-1);
+    }
+};
